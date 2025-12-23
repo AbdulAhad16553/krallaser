abccf6a673bb9ee46f9b9af3a49b9f79524e7b1b
abdullah7834 <rajaahad1010@gmail.com>
2025-12-23 15:01:25 +0500

    update

--- lib/getCustomUrl.ts
+++
@@ -1,7 +0,0 @@
-export const getCustomUrl = (host: any) => {
-    if (host.split(":")[1]) {
-        return process.env.NEXT_PUBLIC_STORE_DOMAIN
-    } else {
-        return host.replace(/^www\./, "");
-    }
-}
